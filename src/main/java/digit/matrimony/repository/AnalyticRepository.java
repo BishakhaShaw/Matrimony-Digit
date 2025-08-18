49aa42a66e70c37cfc702a0d6c82c0d850bad413
Ajay Mehta <Ajay.Mehta@godigit.com>
2025-08-18 22:29:45 +0530

    Your commit message here

--- src/main/java/digit/matrimony/repository/AnalyticRepository.java
+++ src/main/java/digit/matrimony/repository/AnalyticRepository.java
@@ -2,8 +2,6 @@ package digit.matrimony.repository;
 
 import digit.matrimony.entity.Analytic;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface AnalyticRepository extends JpaRepository<Analytic, Long> {
 }
