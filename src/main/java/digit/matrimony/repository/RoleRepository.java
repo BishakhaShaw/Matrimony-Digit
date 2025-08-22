3e432cd8801bca9191f0198e648bd4398279f740
Bishakha Shaw <bishakhakumari111@gmail.com>
2025-08-22 16:16:30 +0530

    removed comments

--- src/main/java/digit/matrimony/repository/RoleRepository.java
+++ src/main/java/digit/matrimony/repository/RoleRepository.java
@@ -1,20 +1,3 @@
-//package digit.matrimony.repository;
-//
-//import digit.matrimony.entity.Role;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import java.util.Optional;
-//
-//public interface RoleRepository extends JpaRepository<Role, Short> {
-//
-//    // ✅ Custom finder by role name
-//    Optional<Role> findByName(String name);
-//}
-
-
-
-
-
 
 package digit.matrimony.repository;
 
